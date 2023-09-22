a3a6cc03a966fdd5a2b5ed0cb33488320f401d06
Priyanshu Trivedi <priyanshutrivedi0081@gmail.com>
2023-09-22 19:05:46 +0530

    digitalclock project folder pushed

---
+++ digitalclock/script.js
@@ -0,0 +1,14 @@
+let hours = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+// console.log(currentTime);
+
+setInterval(() => {
+    let currentTime = new Date();
+    hours.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
+
+// condition ? exprIfTrue : exprIfFalse
